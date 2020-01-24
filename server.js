09fd51f03aadc4ebc4627e4b224c4a8f79c0f9c1
Anurag <mikkydhiman@gmail.com>
2020-01-24 23:07:50 +0530

    Writing the logic for redis cache in the node server:

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("This is the server.js file");
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+// create the express application instance.......
+const app = express();
+
+const client = redis.createClient(6379);
+
+
+client.on('error',(err) => {
+    console.log("Error", err);
+});
+
+app.get('/photos',(req,res) => {
+    // key to store the result in the Redis store......
+    const photosRedisKey = 'user:photos';
+
+    // Try to fetch the data from the redis in case we have cached it.......
+   
